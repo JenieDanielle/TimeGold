5b2cf21a8a69bba511f60653810fd4e8e9dbd5c2
maisamarall <maisaamaral220@gmail.com>
2025-10-01 21:41:45 -0300

    commit das importações

--- src/main.js
+++ src/main.js
@@ -1,9 +1,17 @@
-import { createApp } from 'vue'
+
+import { createApp } from 'vue';
+import PrimeVue from 'primevue/config';
+import Aura from '@primeuix/themes/aura';
 import App from './App.vue'
-import router from './router'
+import router from './router' 
 
-const app = createApp(App)
 
+const app = createApp(App);
 app.use(router)
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura
+    }
+});
 
-app.mount('#app')
+app.mount('#app');
