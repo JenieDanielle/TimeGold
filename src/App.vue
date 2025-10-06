b763c0e9bcfcdd1be3ae6ad0ca3ec8f9fa3473a5
Simone Siqueira de Melo <Sisiqueirakook1997@gmail.com>
2025-10-06 17:07:52 -0300

    Criando a página inicial

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
 <script setup></script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div class="min-h-screen bg-gradient-to-r from-slate-950 via-slate-900 to-slate-950">
+    <div class="">
+      <router-view></router-view>
+    </div>
+  </div>
 </template>
-
-<style scoped></style>
