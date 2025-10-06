4f5fef1b60905daf379f8bf9a318bf7d13355980
Simone Siqueira de Melo <Sisiqueirakook1997@gmail.com>
2025-10-06 20:14:34 -0300

    feat: Adicionando funções de fechamento e abertura do modal

--- src/views/components/ModalComponent.vue
+++ src/views/components/ModalComponent.vue
@@ -1,12 +1,9 @@
 
 <template>
     <div class="card">
-        <div class="flex flex-wrap justify-center gap-2 mb-2">
-            <Button label="TopRight" icon="pi pi-arrow-down-left" @click="openPosition('topright')" severity="secondary" style="min-width: 10rem" />
-        </div>
        
-        <Dialog v-model:visible="visible" header="Edit Profile" :style="{ width: '25rem' }" :position="position" :modal="true" :draggable="false">
-            <span class="text-surface-500 dark:text-surface-400 block mb-8">Update your information.</span>
+        <Dialog v-model:visible="visible" :position="position" header="Edit Profile" :style="{ width: '25rem' }" :modal="true" :draggable="false">
+            <span class="text-surface-500 dark:text-surface-400 block mb-8">Insira seus dados</span>
             <div class="flex flex-col gap-2">
                 <FloatLabel variant="on">
                     <InputText id="on_label" v-model="value3" />
@@ -14,14 +11,17 @@
                 </FloatLabel>
             </div>
             
-
-            <div class="flex items-center gap-4 mb-8">
-                <label for="email" class="font-semibold w-24">Email</label>
-                <InputText id="email" v-model="dados.email" class="flex-auto" autocomplete="off" />
+            <div class="flex flex-col gap-2 mt-5">
+                <FloatLabel variant="on">
+                    <InputText id="on_label" v-model="value3" />
+                    <label for="on_label">Senha</label>
+                </FloatLabel>
             </div>
+
+
             <div class="flex justify-end gap-2">
-                <Button type="button" label="Cancel" severity="secondary" @click="visible = false"></Button>
-                <Button type="button" label="Save" @click="visible = false"></Button>
+                <Button type="button" label="Fechar" severity="secondary" @click="visible = false"></Button>
+                <Button type="button" label="Entrar"></Button>
             </div>
         </Dialog>
     </div>
@@ -29,19 +29,19 @@
 
 <script setup>
 import { ref } from "vue";
-
 import { Button, FloatLabel } from "primevue";
 import {Dialog} from "primevue";
 import { InputText } from 'primevue';
 
-
-const position = ref('center');
-const visible = ref(false);
 const dados = ref([]);
 
-const openPosition = (pos) => {
-    position.value = pos;
-    visible.value = true;
+const visible = ref(false);
+const position = ref('center');
+
+function open(pos = 'center') {
+  position.value = pos;
+  visible.value = true;
 }
 
+defineExpose({ open });
 </script>
